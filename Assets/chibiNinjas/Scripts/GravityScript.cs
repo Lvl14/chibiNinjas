7d18453b4a3a238656edf705d5d0fcf63b10b112
AlexandreAlberti <alberti.bu@gmail.com>
2017-08-06 17:53:26 +0200

    Adding rascador with good gravity corrections

--- Assets/chibiNinjas/Scripts/GravityScript.cs
+++ Assets/chibiNinjas/Scripts/GravityScript.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class GravityScript : MonoBehaviour {
 	public float floorPos = -1000000.0f;
-	public float currvel = 0.0f;
+	private float currvel = 0.0f;
 	private float maxVel = -2.0f;
 	// Use this for initialization
 	void Start () {
@@ -12,13 +12,8 @@ public class GravityScript : MonoBehaviour {
 		pos.y -= 0.1f;
 		RaycastHit2D hitGround = Physics2D.Raycast (pos, Vector2.down, 50.0f);
 		if (hitGround.collider!=null){
-			CircleCollider2D circle = transform.GetComponent<CircleCollider2D> ();
-			if (circle != null) {
-				floorPos = hitGround.point.y + circle.radius;
-			} else {
-				BoxCollider2D box = transform.GetComponent<BoxCollider2D> ();
-				floorPos = hitGround.point.y + box.size.y/2;
-			}
+			BoxCollider2D box = transform.GetComponent<BoxCollider2D> ();
+			floorPos = hitGround.point.y + box.size.y / 2.0f * transform.lossyScale.y;
 		}
 	}
 
@@ -37,18 +32,13 @@ public class GravityScript : MonoBehaviour {
 	}
 
 	void LateUpdate () {
-		Vector3 pos = new Vector3(transform.position.x, transform.position.y > floorPos ? transform.position.y : floorPos, 0);
+		Vector3 pos = new Vector3(transform.position.x, transform.position.y > floorPos ? transform.position.y : floorPos, 0.0f);
 		transform.position = pos;
 		pos.y -= 0.1f;
 		RaycastHit2D hitGround = Physics2D.Raycast (pos, Vector2.down, 50.0f);
 		if (hitGround.collider!=null){
-			CircleCollider2D circle = transform.GetComponent<CircleCollider2D> ();
-			if (circle != null) {
-				floorPos = hitGround.point.y + circle.radius;
-			} else {
-				BoxCollider2D box = transform.GetComponent<BoxCollider2D> ();
-				floorPos = hitGround.point.y + box.size.y/2;
-			}
+			BoxCollider2D box = transform.GetComponent<BoxCollider2D> ();
+			floorPos = hitGround.point.y + box.size.y / 2.0f * transform.lossyScale.y;
 		}
 	}
 }
