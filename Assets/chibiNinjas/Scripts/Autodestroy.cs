552f84cc7a48a88da02103c68c08150e8c35fce8
AlexandreAlberti <alberti.bu@gmail.com>
2017-12-16 08:54:20 +0100

    helpbox

---
+++ Assets/chibiNinjas/Scripts/Autodestroy.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Autodestroy : MonoBehaviour
+{
+	public float timeToDestroy = 10.0f;
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		timeToDestroy -= Time.deltaTime;
+
+		if (timeToDestroy < 0.0f) {
+			Destroy (gameObject);
+		}
+	}
+}
+
