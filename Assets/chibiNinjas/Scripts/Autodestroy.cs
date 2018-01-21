a2e270a9f7248ea3eac944ad284158f5f9a53c04
AlexandreAlberti <alberti.bu@gmail.com>
2018-01-21 17:45:25 +0100

    working full again

--- Assets/chibiNinjas/Scripts/Autodestroy.cs
+++ Assets/chibiNinjas/Scripts/Autodestroy.cs
@@ -4,20 +4,58 @@ using System.Collections;
 public class Autodestroy : MonoBehaviour
 {
 	public float timeToDestroy = 10.0f;
+	private bool activated = true;
+
+	public Autodestroy[] activateOnDestroy;
+	private string[] activateOnDestroyTexts;
 
 	// Use this for initialization
 	void Start ()
-	{
+	{		
+		if (activateOnDestroy != null) {
+			activateOnDestroyTexts = new string[activateOnDestroy.Length];
+			for (int i = 0; i < activateOnDestroy.Length; i++) {
+				Autodestroy item = activateOnDestroy [i];
+				TextMesh tm = item.GetComponent<TextMesh> ();
+				if (tm != null) {
+					activateOnDestroyTexts [i] = tm.text;
+					tm.text = "";
+				}
+				SpriteRenderer sr = item.GetComponent<SpriteRenderer> ();
+				if (sr != null) {
+					sr.enabled = false;
+				}
+			}
+		}
 	
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		timeToDestroy -= Time.deltaTime;
+		if (activated) {
+			timeToDestroy -= Time.deltaTime;
 
-		if (timeToDestroy < 0.0f) {
-			Destroy (gameObject);
+			if (timeToDestroy < 0.0f) {
+				Destroy (gameObject);
+			}
+		}
+	}
+
+	void OnDestroy() {
+		if (activateOnDestroy != null) {
+			for (int i = 0; i < activateOnDestroy.Length; i++) {
+				Autodestroy item = activateOnDestroy [i];
+				TextMesh tm = item.GetComponent<TextMesh> ();
+				if (tm != null) {
+					tm.text = activateOnDestroyTexts [i];
+				}
+				item.activated = true;
+				SpriteRenderer sr = item.GetComponent<SpriteRenderer> ();
+				if (sr != null) {
+					sr.enabled = true;
+				}
+			}
 		}
 	}
 }
--- Assets/chibiNinjas/Scripts/GameManager.cs
+++ Assets/chibiNinjas/Scripts/GameManager.cs
@@ -9,7 +9,7 @@ public class GameManager : MonoBehaviour
 	public static int life;
 	public static int bosslife;
 	public static float[] maxCameraX = 	new float[]	{0.0f,	63.5f,	0.0f, 	76.5f,	49.0f,	129.0f,	700.0f};
-	public static float[] maxCameraY = 	new float[]	{0.0f,	8.0f,	0.0f,	12.0f,	-4.29f,	120.0f,	100.0f};
+	public static float[] maxCameraY = 	new float[]	{0.0f,	8.0f,	0.0f,	9.0f,	-4.29f,	120.0f,	100.0f};
 	public static float[] minCameraY = 	new float[]	{0.0f,	6.0f,	0.0f,	8.0f,	-20.0f,	77.0f,	6.5f};
 
 
